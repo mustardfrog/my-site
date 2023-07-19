a9addd1e733bec0b0a7d6c22577c6dd15715d86a
Frog <frogdoctor@tutanota.com>
2023-07-19 21:42:24 +0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), astroImageTools]
+});
